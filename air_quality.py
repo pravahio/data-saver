02e6b9f92af43b0d1aa4f1ca47fa57cf52dc1466
Abhishek Upperwal <a.upperwal@gmail.com>
2019-12-31 22:23:13 +0530

    init commit

---
+++ air_quality.py
@@ -0,0 +1,24 @@
+import json
+
+from dlake import Datalake
+from mesh_air_quality.main import MeshAirQuality
+from google.protobuf.json_format import MessageToJson
+
+geospace = [
+    '/in/ncr'
+]
+
+def start():
+    c = MeshAirQuality('rpc.pravah.io:5555')
+    feed = c.subscribe(geospace)
+
+    datalake = Datalake('pravah', '', c.get_channel())
+
+    for m, chan in feed:
+        jsonObj = json.loads(MessageToJson(m))
+
+        obj = datalake.insert(chan, jsonObj)
+        print('[AIR_QUALITY] ' + str(obj) + ': ' + chan)
+
+if '__main__' == __name__:
+    start()
---
+++ solar.py
@@ -0,0 +1,24 @@
+import json
+
+from dlake import Datalake
+from mesh_solar_power_production.main import MeshSolarPowerProduction
+from google.protobuf.json_format import MessageToJson
+
+geospace = [
+    '/in/delhi'
+]
+
+def start():
+    c = MeshSolarPowerProduction('rpc.pravah.io:5555')
+    feed = c.subscribe(geospace)
+
+    datalake = Datalake('pravah', '', c.get_channel())
+
+    for m, chan in feed:
+        jsonObj = json.loads(MessageToJson(m))
+
+        obj = datalake.insert(chan, jsonObj)
+        print('[SOLAR] ' + str(obj) + ': ' + chan)
+
+if '__main__' == __name__:
+    start()
